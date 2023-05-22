da6a308308368b4e246842046eaadf8ed5f595ef
gustavo-dm <gustavothemoraes@gmail.com>
2023-05-22 17:17:40 -0300

    projeto final

--- galeria/urls.py
+++ galeria/urls.py
@@ -1,9 +1,7 @@
-from django.contrib import admin
 from django.urls import path
-
-from .views import index, imagem
+from galeria.views import index, imagem
 
 urlpatterns = [
-    path('', index),
-    path('imagem/', imagem),
+    path('', index, name='index'),
+    path('imagem/', imagem, name='imagem'),
 ]
--- galeria/views.py
+++ galeria/views.py
@@ -1,5 +1,4 @@
 from django.shortcuts import render
-from django.http import HttpResponse,HttpRequest
 
 def index(request):
     return render(request, template_name='galeria/index.html')
